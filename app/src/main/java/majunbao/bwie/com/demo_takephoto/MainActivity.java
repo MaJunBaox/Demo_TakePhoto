fd9348cb3c2dfb15b5e8a8e5f9f10ca9c91e5fb2
majunbao <492237446@qq.com>
2018-09-20 14:30:35 +0800

    Initial commit

---
+++ app/src/main/java/majunbao/bwie/com/demo_takephoto/MainActivity.java
@@ -0,0 +1,228 @@
+package majunbao.bwie.com.demo_takephoto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.util.List;
+
+import majunbao.bwie.com.demo_takephoto.utils.TakePhotoUtils;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 10002;
+    private static final int PICK_ACTIVITY_REQUEST_CODE = 10003;
+    private static final int CROP_ACTIVITY_REQUEST_CODE = 10008;
+    private static final int WRITE_EXTERNAL_STORAGE_REQUEST_CODE = 10010;
+
+    private ImageView imageView;
+
+    private String imageFilePath; //拍照和选择照片后图片路径
+    private File cropFile; //裁剪后的图片文件
+    private Uri pickPhotoImageUri; //API22以下相册选择图片uri
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn_pick = (Button) findViewById(R.id.btn_pick);
+        Button btn_take = (Button) findViewById(R.id.btn_take);
+        imageView = (ImageView) findViewById(R.id.image_view);
+        btn_take.setOnClickListener(this);
+        btn_pick.setOnClickListener(this);
+        //打开app检查权限
+        permission();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_pick:
+                pickPhoto();
+                break;
+            case R.id.btn_take:
+                takePhoto();
+                break;
+        }
+    }
+
+
+    //拍照获取图片
+    private void takePhoto() {
+        String state = Environment.getExternalStorageState();
+        if (state.equals(Environment.MEDIA_MOUNTED)) {
+            File imageFile = new File(Environment.getExternalStorageDirectory(), "/temp/" + System.currentTimeMillis() + ".jpg");
+            if (!imageFile.getParentFile().exists()) imageFile.getParentFile().mkdirs();
+            imageFilePath = imageFile.getPath();
+            //兼容性判断
+            Uri imageUri;
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1) {
+                imageUri = TakePhotoUtils.file2Uri(this, imageFile);
+            } else {
+                imageUri = Uri.fromFile(imageFile);
+            }
+            Intent intent = new Intent();
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+            intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);//将拍取的照片保存到指定URI
+
+            List<ResolveInfo> resInfoList = getPackageManager().queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+            for (ResolveInfo resolveInfo : resInfoList) {
+                String packageName = resolveInfo.activityInfo.packageName;
+                grantUriPermission(packageName, imageUri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            }
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+    //从相册中取图片
+    private void pickPhoto() {
+        Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, PICK_ACTIVITY_REQUEST_CODE);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //CropHelper.handleResult(this, requestCode, resultCode, data);
+        switch (requestCode) {
+            case CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE:
+                //拍照
+                if (resultCode == Activity.RESULT_OK) {
+                    crop(false);
+                }
+                break;
+
+            case CROP_ACTIVITY_REQUEST_CODE:
+                //裁剪完成
+                if (data != null) {
+                    Bitmap bitmap;
+                    try {
+                        bitmap = BitmapFactory.decodeFile(cropFile.getPath());
+                        imageView.setImageBitmap(bitmap);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                break;
+
+            case PICK_ACTIVITY_REQUEST_CODE:
+                //从相册选择
+                if (data != null && resultCode == Activity.RESULT_OK) {
+                    if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1) {
+                        imageFilePath = TakePhotoUtils.getPathByUri4kitkat(this, data.getData());
+                    } else {
+                        pickPhotoImageUri = data.getData();
+                    }
+
+                    crop(true);
+                }
+                break;
+        }
+
+    }
+
+
+    /**
+     * 裁剪
+     *
+     * @param isPick 是否是从相册选择
+     */
+    private void crop(boolean isPick) {
+        cropFile = new File(Environment.getExternalStorageDirectory(), "/temp/" + System.currentTimeMillis() + ".jpg");
+        if (!cropFile.getParentFile().exists()) cropFile.getParentFile().mkdirs();
+        Uri outputUri, imageUri;
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1) {
+            outputUri = TakePhotoUtils.file2Uri(this, cropFile);
+            imageUri = TakePhotoUtils.file2Uri(this, new File(imageFilePath));
+        } else {
+            outputUri = Uri.fromFile(cropFile);
+            imageUri = isPick ? pickPhotoImageUri : Uri.fromFile(new File(imageFilePath));
+        }
+
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        intent.setDataAndType(imageUri, "image/*");
+        intent.putExtra("crop", "true");
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+        intent.putExtra("scale", true);
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, outputUri);
+        intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());
+        intent.putExtra("noFaceDetection", true); // no face detection
+
+        //授予"相机"保存文件的权限 针对API24+
+        List<ResolveInfo> resInfoList = getPackageManager().queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+        for (ResolveInfo resolveInfo : resInfoList) {
+            String packageName = resolveInfo.activityInfo.packageName;
+            grantUriPermission(packageName, outputUri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        }
+        startActivityForResult(intent, CROP_ACTIVITY_REQUEST_CODE);
+    }
+
+
+    /**
+     * 申请权限
+     */
+    private void permission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                //拒绝权限以后
+                showMessageOKCancel();
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        WRITE_EXTERNAL_STORAGE_REQUEST_CODE);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == WRITE_EXTERNAL_STORAGE_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //同意授权
+            } else {
+                //拒绝授权后重新申请
+                permission();
+            }
+        }
+    }
+
+
+    private void showMessageOKCancel() {
+        new AlertDialog.Builder(MainActivity.this)
+                .setMessage("必须授予储存空间的权限！！！！！！！！！！！！！！")
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                })
+                .create()
+                .show();
+    }
+
+
+}
